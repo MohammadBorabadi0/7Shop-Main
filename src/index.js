ff4a231a9e975d14794aeb6ed0684b49c4a2600e
MohammadBorabadi0 <MohammadBorabadi0@gmail.com>
2022-06-17 20:54:03 -0700

    added ProductsProvider to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// Context 
+import ProductsProvider from "./Providers/Context/products_context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <ProductsProvider>
     <App />
+  </ProductsProvider>
 );
