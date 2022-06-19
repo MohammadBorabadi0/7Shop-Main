50d2649ae8165e9558b94711d1ea3ccccd74aef9
MohammadBorabadi0 <MohammadBorabadi0@gmail.com>
2022-06-19 05:43:02 -0700

    added FavoriteList & FavoriteItem to project

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import ProductList from "./Components/ProductList";
 import ProductDetail from "./Components/ProductDetail";
 import Cart from "./Components/Cart";
+import FavoriteList from "./Components/FavoriteList";
 
 const App = () => {
   return (
     <>
       <ToastContainer />
       <Routes>
+        <Route path="favorites" element={<FavoriteList />} />
         <Route path="cart" element={<Cart />} />
         <Route path="product/:id" element={<ProductDetail />} />
         <Route path="/" element={<ProductList />} />
