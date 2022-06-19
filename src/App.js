1263d6f3d4489fb28371c4467b583bd740253617
MohammadBorabadi0 <MohammadBorabadi0@gmail.com>
2022-06-19 00:43:04 -0700

    added Cart to project

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React from "react";
 
 // Toastify
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 // React-Router-Dom
 import { Route, Routes } from "react-router-dom";
+// Context 
 import ProductList from "./Components/ProductList";
 import ProductDetail from "./Components/ProductDetail";
+import Cart from "./Components/Cart";
 
 const App = () => {
   return (
     <>
       <ToastContainer />
       <Routes>
-        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="product/:id" element={<ProductDetail />} />
         <Route path="/" element={<ProductList />} />
       </Routes>
     </>
